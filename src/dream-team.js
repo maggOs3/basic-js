d0afef39a9ce96277847a9444fcf0c595ed61fdf
Max <maxbelykh2001@gmail.com>
2022-10-16 16:49:39 +0300

    feat: make cat:

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,23 +1,30 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Create name of dream team based on the names of its members
- *  
- * @param {Array} members names of the members 
+ *
+ * @param {Array} members names of the members
  * @return {String | Boolean} name of the team or false
  * in case of incorrect members
  *
  * @example
- * 
+ *
  * createDreamTeam(['Matt', 'Ann', 'Dmitry', 'Max']) => 'ADMM'
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function createDreamTeam(members) {
+  // let dtName = [];
+  // if ()
+  // for (let name = 0; name < members.length; name++) {
+  //   if (typeof members[name] === "string") {
+  //     dtName.push(members[name][0]);
+  //   }
+  // }
+  // dtName = dtName.sort().join("");
+  // return dtName.toUpperCase();
 }
 
 module.exports = {
-  createDreamTeam
+  createDreamTeam,
 };
